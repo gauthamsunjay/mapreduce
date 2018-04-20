177929a04755836523de838af909032f3ab4503a
Gautham Sunjay <gautham@cs.wisc.edu>
2018-04-20 16:30:55 -0500

    Simple array implementation

---
+++ mapreduce.c
@@ -0,0 +1,214 @@
+#include<pthread.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include "mapreduce.h"
+
+//definitions
+#define MIN_KEYS_PER_PARTITION 50
+
+typedef struct _kv {
+    char *key;
+    char *value;
+} kv;
+
+typedef struct _reducer_args {
+    int partition;
+    Reducer reduce;
+} rargs;
+
+pthread_mutex_t file_lock;
+int total_files;
+char **fileq;
+Partitioner p_fn;
+int NUM_PARTITIONS;
+
+kv ***partitions;
+int *last_index_per_partition;
+pthread_mutex_t *partition_locks;
+int *iterator_indices;
+
+unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *key++) != '\0')
+        hash = hash * 33 + c;
+    return hash % num_partitions;
+}
+
+char *get_next(char *key, int partition_number) {
+    int idx = iterator_indices[partition_number];
+    if (idx <= last_index_per_partition[partition_number]) {
+        kv *kv_pair = partitions[partition_number][idx];
+        if (strcmp(kv_pair->key, key) == 0) {
+            iterator_indices[partition_number]++;
+            return kv_pair->value;
+        }
+    }
+    return NULL;
+}
+
+void insert_kv(char *key, char *value, int partition) {
+    kv *kv_pair = (kv *) malloc(sizeof(kv));
+    kv_pair->key = key;
+    kv_pair->value = value;
+    
+    pthread_mutex_lock(&partition_locks[partition]);
+    if ((last_index_per_partition[partition] + 1) > 0 && (last_index_per_partition[partition] + 1) % MIN_KEYS_PER_PARTITION == 0) {
+        partitions[partition] = realloc(partitions[partition], (last_index_per_partition[partition] + 1 + MIN_KEYS_PER_PARTITION) * sizeof(kv *));
+    }
+    partitions[partition][++last_index_per_partition[partition]] = kv_pair;
+    pthread_mutex_unlock(&partition_locks[partition]);
+}
+
+void MR_Emit(char *k, char *v) {
+    char *key = (char *) malloc(sizeof(char) * strlen(k));
+    char *val = (char *) malloc(sizeof(char) * strlen(v));
+    key = strcpy(key, k);
+    val = strcpy(val, v);
+    int partition = p_fn(key, NUM_PARTITIONS);
+    insert_kv(key, val, partition);
+}
+
+int compare_keys(const void *p, const void *q) {
+    kv *kv1 = *((kv **) p);
+    kv *kv2 = *((kv **) q);
+    return strcmp(kv1->key, kv2->key);
+}
+
+void *sorter(void *arg) {
+    int partition = *((int *) arg);
+    qsort(partitions[partition], last_index_per_partition[partition] + 1, sizeof(kv *), compare_keys);
+    return NULL;
+}
+
+void sort_keys() {
+    int i;
+    pthread_t sorter_threads[NUM_PARTITIONS];
+    int partitions[NUM_PARTITIONS];
+    for (i = 0; i < NUM_PARTITIONS; i++) {
+        partitions[i] = i;
+        pthread_create(&sorter_threads[i], NULL, sorter, &partitions[i]);
+    }
+
+    for (i = 0; i < NUM_PARTITIONS; i++) {
+        pthread_join(sorter_threads[i], NULL);
+    }
+}
+
+char* fetch_file() {
+    static int cfile = 0;
+    int i;
+    pthread_mutex_lock(&file_lock);
+    i = ++cfile;
+    pthread_mutex_unlock(&file_lock);
+    if (i >= total_files)
+        return NULL;
+    return fileq[i];
+}
+
+void *map_thread(void *arg) {
+    Mapper map = (Mapper) arg;
+    while (1) {
+        char* file = fetch_file();
+        if (file == NULL)
+            return NULL;
+        map(file);
+    }
+}
+
+void execute_mappers(Mapper map, int num_mappers) { 
+    int i;
+    pthread_t map_threads[num_mappers];
+    for (i = 0; i < num_mappers; i++) {
+        pthread_create(&map_threads[i], NULL, map_thread, map);
+    }
+   
+    for (i = 0; i < num_mappers; i++) {
+        pthread_join(map_threads[i], NULL);
+    }
+}
+
+void *reducer_thread(void *args) {
+    rargs *_rargs = (rargs *) args;
+    if (last_index_per_partition[_rargs->partition] >= 0) {
+        while (1) {
+            if (iterator_indices[_rargs->partition] > last_index_per_partition[_rargs->partition])
+                break;
+
+            kv *kvp = partitions[_rargs->partition][iterator_indices[_rargs->partition]];
+            _rargs->reduce(kvp->key, get_next, _rargs->partition);
+        }
+    }
+    return NULL;
+}
+
+void execute_reducers(Reducer reduce) {
+    int i;
+    pthread_t reducer_threads[NUM_PARTITIONS];
+    rargs args[NUM_PARTITIONS];
+    for (i = 0; i < NUM_PARTITIONS; i++) {
+        args[i].partition = i;
+        args[i].reduce = reduce;
+        pthread_create(&reducer_threads[i], NULL, reducer_thread, &args[i]);
+    }
+    
+    for (i = 0; i < NUM_PARTITIONS; i++) {
+        pthread_join(reducer_threads[i], NULL);
+    }
+}
+
+void initialize(int argc, char *argv[], int num_reducers, Partitioner partition) {
+    total_files = argc;
+    fileq = argv;
+    p_fn = partition;
+    NUM_PARTITIONS = num_reducers;
+
+    partitions = (kv ***) malloc(NUM_PARTITIONS * sizeof(kv **));
+    last_index_per_partition = (int *) malloc(NUM_PARTITIONS * sizeof(int));
+    partition_locks = (pthread_mutex_t *) malloc(NUM_PARTITIONS * sizeof(pthread_mutex_t));
+    iterator_indices = (int *) malloc(NUM_PARTITIONS * sizeof(int));
+
+    int i;
+    for (i = 0; i < NUM_PARTITIONS; i++) {
+        partitions[i] = (kv **) malloc(MIN_KEYS_PER_PARTITION * sizeof(kv *));
+        last_index_per_partition[i] = -1;
+        iterator_indices[i] = 0;
+    }
+}
+
+void print_values() {
+    int i, j;
+    for (i = 0; i < NUM_PARTITIONS; i++) {
+        printf("Partition = %d\n", i);
+        for (j = 0; j <= last_index_per_partition[i]; j++) {
+            printf("Key = %s, value = %s\n", partitions[i][j]->key, partitions[i][j]->value);
+        }
+    }
+}
+
+void cleanup() {
+    int i, j;
+    for (i = 0; i < NUM_PARTITIONS; i++) {
+        for (j = 0; j <= last_index_per_partition[i]; j++) {
+            free(partitions[i][j]);
+        }
+        free(partitions[i]);
+    }
+    free(last_index_per_partition);
+    free(iterator_indices);
+}
+
+void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, 
+            Reducer reduce, int num_reducers, Partitioner partition) {
+    
+    initialize(argc, argv, num_reducers, partition);
+    execute_mappers(map, num_mappers);
+    /*printf("--------------------\n");
+    print_values();*/
+    sort_keys();
+    /*printf("--------------------\n");
+    print_values();*/
+    execute_reducers(reduce);
+    cleanup();
+}
---
+++ word_count.c
@@ -0,0 +1,35 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "mapreduce.h"
+
+void Map(char *file_name) {
+    FILE *fp = fopen(file_name, "r");
+    assert(fp != NULL);
+
+    char *line = NULL;
+    size_t size = 0;
+    while (getline(&line, &size, fp) != -1) {
+        char *token;
+        char *dummy = line;
+        while ((token = strsep(&dummy, " \n")) != NULL) {
+            MR_Emit(token, "1");
+        }
+    }
+    free(line);
+    fclose(fp);
+}
+
+void Reduce(char *key, Getter get_next, int partition_number) {
+    int count = 0;
+    char *value;
+    while ((value = get_next(key, partition_number)) != NULL)
+        count++;
+    printf("%s %d\n", key, count);
+}
+
+int main(int argc, char *argv[]) {
+    MR_Run(argc, argv, Map, 1, Reduce, 1, MR_DefaultHashPartition);
+    return 1;
+}
